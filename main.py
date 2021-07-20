4d1acdb8ac1cd6d4889d2d2c512a336d94136e68
ivn-guerrero <ivn.guerrero@yahoo.com>
2021-07-20 08:00:31 -0500

    resize the image so that it can fit in the terminal window

--- main.py
+++ main.py
@@ -12,25 +12,30 @@ from utils import (
 )
 
 CHAR_MAP = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+MIN_BRIGHTNESS = 0
+MAX_BRIGHTNESS = 255
+MIN_CHAR_IDX = 0
+MAX_CHAR_IDX = len(CHAR_MAP) - 1
 
 try:
     im = Image.open("./input.jpg")
-    width, height = im.size
+    term_size = os.get_terminal_size()
+    t_width, t_height = term_size.columns, term_size.lines
+    im.thumbnail((t_width // 3, t_height))
+    f_width, f_height = im.size
     os.system("cls" if os.name == "nt" else "clear")
-    print("Successfully loaded image!")
-    print(f"Image size: {width} x {height}")
 except:
     print("Couldn't load the image")
     sys.exit(-1)
 
-pixels = unflatten(list(im.getdata()), height, width)
+pixels = unflatten(list(im.getdata()), f_height, f_width)
 brightness_matrix = map_to_brightness_avg(pixels)
 char_matrix = map_to_chars(
     brightness_matrix,
     CHAR_MAP,
-    0,
-    255,
-    0,
-    len(CHAR_MAP) - 1
+    MIN_BRIGHTNESS,
+    MAX_BRIGHTNESS,
+    MIN_CHAR_IDX,
+    MAX_CHAR_IDX
 )
 print_ascii_art(char_matrix)
